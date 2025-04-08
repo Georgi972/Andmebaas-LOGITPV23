69ab5777f47d5315eb472ab2307cabb3cfa26cb5
Georgi972 <Georgi.razinkov@gmail.com>
2025-04-08 13:12:13 +0300

    varskenda terve sql

--- SQLlaused.sql
+++ SQLlaused.sql
@@ -31,6 +31,7 @@ drop table inimene;
 insert into inimene(nimi, synniaeg, telefon, pikkus, opilaskodu)
 values
 ('Mati Kaskk', '2021-12-30', '2568952', 90.5, 0),
+('Anna Kivi', '2011-12-30', '2564952', 98.5, 0),
 ('endru jak', '2022-12-30', '2562552', 90.5, 0),
 ('anno kivi', '2012-12-30', '2568122', 150.5, 0),
 ('ants pärt', '2010-12-30', '2545952', 245.5, 0);
@@ -39,4 +40,69 @@ select * from inimene;
 
 
 delete from inimene
-where inimeneID=3;
+where inimeneID=4;
+--tabel elukoht
+create table elukoht(
+elukohtID int primary key identity(1,1),
+elukoht varchar(50) unique,
+maakond varchar(50)
+);
+select * from elukoht;
+--andmete lisamine tabeli elukoht
+insert into elukoht(elukoht,maakond)
+values ('Tartu','Tartumaa'),
+('Pärnu','Pärnumaa');
+--tabeli muutmine uue veergu lisamine
+alter table inimene add elukohtID int;
+select * from inimene;
+--foreignh key lisamine
+alter table inimene
+add constraint fk_elukoht
+foreign key (elukohtID) 
+references elukoht(elukohtID);
+
+select * from inimene;
+select * from elukoht;
+
+insert into inimene
+(nimi, synniaeg, telefon, pikkus, opilaskodu, elukohtID)
+values
+('Mati suur', '2021-12-30', '2568952', 90.5, 0, 3);
+
+select inimene.nimi, inimene.synniaeg, elukoht.elukoht 
+from inimene join elukoht
+on inimene.elukohtID=elukoht.elukohtID
+
+select i.nimi, i.synniaeg, e.elukoht 
+from inimene i join elukoht e
+on i.elukohtID=e.elukohtID
+
+create table auto(
+autoID int primary key identity(1,1),
+autonr varchar(7) unique,
+mudell varchar(50),
+mark varchar(50),
+vaast char(10)
+);
+
+alter table inimene add autoID int;
+alter table inimene
+add constraint fk_auto
+foreign key (autoID)
+references auto(autoID);
+
+select * from auto;
+
+INSERT INTO auto
+(autonr, mudell, mark, vaast)
+VALUES ('turbos', 'tunning', 'porche', '2012-3-5')
+
+INSERT INTO inimene
+(nimi, synniaeg, telefon, pikkus, opilaskodu, elukohtID, autoID)
+VALUES ('turbo2', '2001-12-30', '091236543', 10.00, 0, 1, 1)
+
+select i.nimi, a.vaast, a.mark, a.mudell
+from inimene i join auto a
+on i.elukohtID=a.autoID;
+delete from auto
+where autoID=1;
